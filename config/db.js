fe0850c7a501180babbf794ec94160297ef0c553
Adithyan <adithyanadi408@gmail.com>
2026-01-14 23:53:30 +0530

    Added

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI)
-    } catch (error) {
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    process.exit(1);
+  }
+};
